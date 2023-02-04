07a3380721f0470ca21184ff74fa7d5d389ec266
AlexForward98 <114426859+AlexForward98@users.noreply.github.com>
2023-02-04 18:30:08 +0000

    Add files via upload

--- battleships/scripts/index.js
+++ battleships/scripts/index.js
@@ -1,31 +1,31 @@
 const player1Table = document.getElementById('player1-table');
 
-player1Table.addEventListener("click", (e) => {
+player1Table.addEventListener('click', (e) => {
   const clickedBtn = e.target;
   // const image = clickedBtn.firstChild;
   // image.classList.add("fa-xmark");
 
-  const i = document.createElement("i");
-  i.classList.add("fa-solid");
-  i.classList.add("fa-xmark");
+  const i = document.createElement('i');
+  i.classList.add('fa-solid');
+  i.classList.add('fa-xmark');
   clickedBtn.append(i);
 });
 
-function randomNumberLetter(){
-  let x = Math.floor((Math.random() * 10));
+function randomNumberLetter() {
+  let x = Math.floor(Math.random() * 10);
   return x;
 }
 
-function randomNumber(){
-  let x = Math.floor((Math.random() * 10) + 1);
+function randomNumber() {
+  let x = Math.floor(Math.random() * 10 + 1);
   return x;
 }
 
-function generateLetterShip(){
+function generateLetterShip() {
   const num = randomNumberLetter();
-  const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
   const position = [];
-  if(num === 9 || num === 8){
+  if (num === 9 || num === 8) {
     position.push(letters[num]);
     position.push(letters[num - 1]);
     position.push(letters[num - 2]);
@@ -37,10 +37,10 @@ function generateLetterShip(){
   return position;
 }
 
-function generateNumberShip(){
+function generateNumberShip() {
   const num = randomNumber();
   const position = [];
-  if(num === 10 || num === 9){
+  if (num === 10 || num === 9) {
     position.push(num);
     position.push(num - 1);
     position.push(num - 2);
@@ -57,13 +57,13 @@ class VerticalShip {
   number = randomNumber();
 }
 
-const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
 class HorizontalShip {
   letter = letters[randomNumber()];
   numbers = generateNumberShip();
 }
 
-console.log(new VerticalShip);
-console.log(new VerticalShip);
-console.log(new HorizontalShip);
+console.log(new VerticalShip());
+console.log(new VerticalShip());
+console.log(new HorizontalShip());
